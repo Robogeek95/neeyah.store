bab9b6a8ba684ad702a3f06f766d1953c6c4ec0e
robogeek95 <azeezlukman95@gmail.com>
2021-11-22 11:22:08 +0100

    added basic slideshow

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import { useState } from 'react';
-import { ProductCard } from '@/components/index';
+import { useState } from "react";
+import { ProductCard } from "@/components/index";
 
-import products from 'products';
+import products from "products";
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   const [disabled, setDisabled] = useState(false);
 
   return (
-    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
+    <div className="container xl:max-w-screen-xl mx-auto pb-12 px-6">
+      <div className="mb-10">
+        <Carousel />
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {products.map(product => (
+        {products.map((product) => (
           <ProductCard
             key={product.id}
             disabled={disabled}
